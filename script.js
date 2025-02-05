2497e0db0bf0cd7309c7746b68bcfe9fcd6bd3c0
Mauricio <mauriciohillesheim15@gmail.com>
2025-02-05 19:18:02 -0300

    If-Else

---
+++ script.js
@@ -0,0 +1,59 @@
+// let num1 = 10;
+// let num2 = 5;
+
+// if (num1 > num2) {
+//     //O código que vai ser executado se a condição do IF for satisfeita
+//   console.log('Número 1 é maior que número 2');
+// } 
+
+// else if(num1 === num2){
+//     // Executar o código, caso a condição do IF for igual nas duas variaveis
+//     console.log('Número 1 é igual ao número 2');
+// }
+// else if(num1 < num2){
+//     // Executar o código, caso a condição do IF não for satisfeita
+//     console.log('Número 1 é menor que número 2')
+// }
+
+// let mum1maior;
+
+// if (num1 > num2) {
+//     num1maior = true;
+// } else{
+//     num1maior = false;
+// }
+
+// console.log(num1maior);
+
+// num1maior = num1 > num2 ? true : false;
+
+// console.log(num1maior);
+
+// let cidade = "São Paulo";
+
+// if(cidade === "São Paulo"){
+//     console.log("A cidade selecionada é São Paulo")
+// }
+
+// else if(cidade === "Curitiba"){
+//     console.log("A cidade selecionada é Curitiba")
+// }
+
+// else if(cidade === "Florianópolis"){
+//     console.log("A cidade selecionada é Florianópolis")
+// }
+
+// let cidade = "São Paulo";
+
+// switch(cidade){
+//     case "São Paulo":
+//         console.log("A cidade selecionada é São Paulo");
+//         break;
+//     case "Curitiba":
+//         console.log("A cidade selecionada é Curitiba");
+//         break;
+//     case "Florianópolis":
+//         console.log("A cidade selecionada é Florianópolis");
+//         break;
+// }
+
